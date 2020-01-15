3fcc8fa078d41803481b7b9385a65ffe95b27423
Miha Jamsek <miha_jamsek@windowslive.com>
2020-01-15 23:26:35 +0100

    added gcloud integration

---
+++ api/v1/src/main/java/si/rso/invoice/api/listeners/StartupContextListener.java
@@ -0,0 +1,50 @@
+package si.rso.invoice.api.listeners;
+
+import com.kumuluz.ee.configuration.utils.ConfigurationUtil;
+import com.kumuluz.ee.logs.LogManager;
+import com.kumuluz.ee.logs.Logger;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
+
+@ApplicationScoped
+public class StartupContextListener implements ServletContextListener {
+    
+    public static final Logger LOG = LogManager.getLogger(StartupContextListener.class.getSimpleName());
+    
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        String credentials = ConfigurationUtil.getInstance()
+            .get("google.storage.credentials")
+            .orElseThrow(() -> new RuntimeException("Google credentials not set!"));
+        
+        LOG.info("Decoding credentials...");
+        String decodedCredentials = new String(Base64.getDecoder().decode(credentials));
+        LOG.info("Credentials decoded!");
+        
+        try {
+            if (!Files.exists(Paths.get("credentials"))) {
+                Files.createDirectory(Paths.get("credentials"));
+            }
+            LOG.info("Creating credentials file...");
+            FileWriter fileWriter = new FileWriter(new File("credentials/google-key.json"));
+            fileWriter.write(decodedCredentials);
+            fileWriter.close();
+            LOG.info("Credentials file created!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+    
+    }
+}
